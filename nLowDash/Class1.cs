21f89aedfdb0c424b0ce72e7a883744462f4dfee
walt <2963975+waltiam@users.noreply.github.com>
2018-11-09 14:02:33 -0500

    added Using

---
+++ nLowDash/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace nLowDash
+{
+    public static partial class _
+    {
+        public static TResult Using<TDisposable, TResult>(
+            TDisposable disposable,
+            Func<TDisposable, TResult> function)
+        where TDisposable : IDisposable
+        {
+            using(disposable) return function(disposable);
+        }
+    }
+}
