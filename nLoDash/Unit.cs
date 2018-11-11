44c8efa46b44de34d71cade6ba4ec97e745b1f69
walt <2963975+waltiam@users.noreply.github.com>
2018-11-11 11:18:27 -0500

    fixed an error in the code

--- nLoDash/Unit.cs
+++ nLoDash/Unit.cs
@@ -1,4 +1,5 @@
 using Unit = System.ValueTuple;
+using System;
 
 namespace Floatingman.nLoDash {
     using static _;
@@ -9,11 +10,11 @@ namespace Floatingman.nLoDash {
 
     public static class ActionExtensions {
 
-        public static Func<Unit> ToFunc (this Action action) => () => { action (); return Unit (); }
+        public static Func<Unit> ToFunc (this Action action) => () => { action (); return Unit (); };
 
-        public static Func<T, Unit> ToFunc (this Action<T> action) => (t) => { action (t); return Unit (); }
+        public static Func<T, Unit> ToFunc<T> (this Action<T> action) => (t) => { action (t); return Unit (); };
 
-        public static Func<T1,T2, Unit> ToFunc (this Action<T1,T2> action) => (t1,t2) => { action (t1,t2); return Unit (); }
+        public static Func<T1,T2, Unit> ToFunc<T1,T2> (this Action<T1,T2> action) => (t1,t2) => { action (t1,t2); return Unit (); };
 
         // etc. add new Func overloads as required
     }
