8a2e30ae554efa8145ec53d2308e0873de5ace24
walt <2963975+waltiam@users.noreply.github.com>
2018-11-11 13:32:56 -0500

    adding a sample project to show usage

---
+++ nLoDash.Sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nLoDash.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
